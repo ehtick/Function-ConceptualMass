5fdf0990d4e7e45ac4f15668d6ed743a9845a099
Dmytro Muravskyi <dmuravskiy@amcbridge.com>
2024-01-17 12:13:03 +0200

    Remove unused function that failed build

--- dependencies/LevelGroup.cs
+++ dependencies/LevelGroup.cs
@@ -24,32 +24,6 @@ namespace Elements
             return Levels.Where(l => l.Elevation < height + 0.01).Skip(offset).ToList();
         }
 
-        public List<Level> GetNLevels(int count, int offset = 0)
-        {
-            var levelElements = new List<Level>();
-            for (int i = 0; i < Levels.Count; i++)
-            {
-                var level = Levels[i];
-                level.Index = i;
-                if (level.Height != null)
-                {
-                    levelElements.Add(level);
-                }
-            }
-            var levels = levelElements.Skip(offset).Take(count).ToList();
-            if (levels.Count < count)
-            {
-                CreateEnvelopes.Logging.LogWarning($"A mass was set to have {count} Levels, but there were not enough levels defined in the level group. The last valid level is being repeated.");
-            }
-            while (levels.Count < count)
-            {
-                var lastLevel = levels.LastOrDefault() ?? levelElements.LastOrDefault();
-                var levelToAdd = new Level(lastLevel.Elevation + lastLevel.Height ?? 3, 3) { Name = "Unspecified Level" };
-                levels.Add(levelToAdd);
-            }
-            return levels;
-        }
-
         public List<Level> GetLevelsBetween(Level bottomLevel, Level topLevel)
         {
             if (Levels.Count < 2)
